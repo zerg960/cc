f56ee4a2247b17b1ea867b28f4edc41aa256f463
kreatious.net <shotzo@gmail.com>
2025-08-22 00:00:51 -0500

    Add print function to startup.lua parameters

--- startup.lua
+++ startup.lua
@@ -1,4 +1,4 @@
-function(http, textutils, peripheral, os, pairs, require, parallel)
+function(http, textutils, peripheral, os, pairs, require, parallel, print)
   local songs = {}
   
   for _, title in pairs(textutils.unserialize(http.get("https://raw.githubusercontent.com/zerg960/cc/refs/heads/main/index.txt").readAll())) do
